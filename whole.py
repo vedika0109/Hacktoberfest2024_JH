ca8bcf3f4d9d12a65168b866de6bcccbf4d215ac
Vedika Choudhary <118730174+vedika0109@users.noreply.github.com>
2024-10-23 02:56:46 +0530

    Create whole.py

---
+++ whole.py
@@ -0,0 +1,7 @@
+def is_whole_number_method1(number):
+    return number.is_integer()
+
+# Example
+float_value = 7.0
+result = is_whole_number_method1(float_value)
+print(f&quot;{float_value} is a whole number: {result}&quot;)
